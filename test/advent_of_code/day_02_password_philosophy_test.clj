5cacc0b8481af0c2ec56b294ecfa6be806c07c3f
Andrei Ursan <hello@andreiursan.com>
2020-12-03 10:14:43 +0100

    chore: Fix test typo

--- test/advent_of_code/day_02_password_philosophy_test.clj
+++ test/advent_of_code/day_02_password_philosophy_test.clj
@@ -5,7 +5,8 @@
 (def password-strings ["1-3 a: abcde"
                        "1-3 b: cdefg"
                        "2-9 c: ccccccccc"])
-(deftest test-report
+
+(deftest test-password-validation
   (testing "Find the number of valid passwords"
     (is (= 2 (count-valid-passwords-part1 password-strings)))
     (is (= 1 (count-valid-passwords-part2 password-strings)))))
